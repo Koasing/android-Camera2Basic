067a068902153d47b2663014a8946b29745758a9
Tomoaki Imai <tomoima525@gmail.com>
2018-11-18 22:34:43 -0800

    randomly change filters

--- kotlinApp/Application/src/main/java/com/example/android/camera2basic/opengl/ColorShader.kt
+++ kotlinApp/Application/src/main/java/com/example/android/camera2basic/opengl/ColorShader.kt
@@ -3,13 +3,17 @@ package com.example.android.camera2basic.opengl
 import android.content.Context
 import android.graphics.SurfaceTexture
 import android.opengl.GLES20
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import kotlin.math.absoluteValue
 
 class ColorShader(
         context: Context, texture: SurfaceTexture, width: Int, height: Int
 ): CameraRenderer(context, texture, width, height, "touchcolor.frag.glsl", "touchcolor.vert.glsl") {
-    private var offsetR = 0.5f
-    private var offsetG = 0.5f
-    private var offsetB = 0.5f
+    private var offsetR = 1.0f
+    private var offsetG = 1.0f
+    private var offsetB = 1.0f
 
     /**
      * we override [.setUniformsAndAttribs] and make sure to call the super so we can add
@@ -38,5 +42,42 @@ class ColorShader(
         offsetR = rawX / surfaceWidth
         offsetG = rawY / surfaceHeight
         offsetB = offsetR / offsetG
+    }
+
+    private var valueRed: Float = 0f
+    private var valueGreen: Float = 0f
+    private var thread: HandlerThread? = null
+    private var handler: Handler? = null
+    private var isRunning: Boolean = false
+    fun randomlyChangeColor() {
+        thread = HandlerThread("shader").also {
+            it.start()
+            isRunning = true
+            handler = Handler(it.looper).apply {
+                post {
+                    while(isRunning) {
+                        offsetR = ((surfaceWidth - valueRed).absoluteValue % surfaceWidth) / surfaceWidth
+                        offsetG = ((surfaceHeight - valueGreen).absoluteValue % surfaceHeight) / surfaceHeight
+                        offsetB = offsetR / offsetG
+                        valueGreen += 100
+                        valueRed += 100
+                        sleep(100)
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun stopRandomColorChange() {
+        isRunning = false
+        thread?.quit()
+        thread = null
+        handler = null
+        offsetR = 1.0f
+        offsetG = 1.0f
+        offsetB = 1.0f
+        valueGreen = 0f
+        valueRed = 0f
     }
 }
